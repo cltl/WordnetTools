2586c7f980ddb7b6f682aec759959cbf7a3895c9
piekvossen <piek.vossen@vu.nl>
2019-07-12 11:55:55 +0200

    -- api for OpenMultilingualWordnets that can be loaded on top of WN-LMF

--- scripts/omwn-sim.sh
+++ scripts/omwn-sim.sh
@@ -1,4 +1,6 @@
 #OMWN
-#
+# --target 龙葵属植物 --source 龙虎草 -
+SOURCE=$1
+TARGET=$2
 #ALL
-java -Xmx812m -cp ../lib/WordnetTools-1.0-jar-with-dependencies.jar vu.wntools.wnsimilarity.main.WordSimOmwn --wnlmf ../resources/wneng-30.lmf.xml --omwn ../resources/cow/wn-data-cmn.tab --wncol 0 --lemmacol 2 --target 龙葵属植物 --source 龙虎草 --prefix eng-30-
+java -Xmx812m -cp ../target/WordnetTools-v3.0-jar-with-dependencies.jar vu.wntools.wnsimilarity.main.WordSimOmwn --wnlmf ../resources/pwn/wneng-30.lmf.xml --omwn ../resources/cow/wn-data-cmn.tab --wncol 0 --lemmacol 2 --source $SOURCE --target $TARGET --prefix eng-30-
